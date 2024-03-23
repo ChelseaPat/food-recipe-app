4882fa54ab3ff677ce9dd57426202cda224b3086
Chelsea Patterson <chelseapat77@gmail.com>
2024-03-23 21:24:03 +1300

    Render list of recipes returned from search submit

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/recipe-item";
+
 export default function Home() {
+    const { recipeList, loading } = useContext(GlobalContext);
+
+    if (loading) return <div>Loading... Please wait</div>
+
     return (
-        <div>Home</div>
+        <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+            {
+                recipeList && recipeList.length > 0 ?
+                recipeList.map((item) => 
+                    <RecipeItem item={item} />
+                )
+                : <div>
+                    <p className="lg:text-4xl text-xl text-center text-black font-extrabolder">
+                        Nothing to show here 👀... 
+                    </p>
+                </div>
+            }
+        </div>
     );
 }
