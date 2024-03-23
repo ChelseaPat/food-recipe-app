b51450dcd6fa2bc034391b5ad0d94e86bb9b771b
Chelsea Patterson <chelseapat77@gmail.com>
2024-03-23 21:01:25 +1300

    On submit search, fetch the data matching search

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -5,5 +5,16 @@ export const GlobalContext = createContext(null);
 export default function GlobalState({children}) {
     const [searchParam, setSearchParam] = useState('');
 
-    return <GlobalContext.Provider value={{ searchParam, setSearchParam }}>{children}</GlobalContext.Provider>;
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const resp = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`)
+
+            const data = await resp.json();
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return <GlobalContext.Provider value={{ searchParam, setSearchParam, handleSubmit }}>{children}</GlobalContext.Provider>;
 }
